124d1ba6d5bbfd2ef099744b1dc055989dd2733f
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-21 16:56:22 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white p-2 m-2'>App</div>
+  )
+}
+
+export default App
